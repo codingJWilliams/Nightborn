9a6e5a8f2b9beab31ce61f4511b932845d59ac70
Jay Williams <course@jaywilliams.me>
2017-11-04 18:49:23 +0000

    Change path

--- helpers/setFlag.js
+++ helpers/setFlag.js
@@ -1,6 +1,6 @@
 module.exports = function setFlag(flagNameString, valueBool) {
     var path = require("path");
-    var flagsPath = path.join(".", "..", "..", "CheckUserBot", "storage", "flags.json");
+    var flagsPath = path.join(".", "..", "CheckUserBot", "storage", "flags.json");
     var fs = require("fs");
     var flags = JSON.parse(fs.readFileSync(flagsPath));
     flags[flagNameString] = valueBool;
