10d042a07bd8de665ecf288a0c294cb780a93b08
Jay Williams <course@jaywilliams.me>
2017-11-04 18:54:38 +0000

    G

--- helpers/setFlag.js
+++ helpers/setFlag.js
@@ -2,7 +2,9 @@ module.exports = function setFlag(flagNameString, valueBool) {
     var path = require("path");
     var flagsPath = path.join(".", "..", "CheckUserBot", "storage", "flags.json");
     var fs = require("fs");
+
     var flags = JSON.parse(fs.readFileSync(flagsPath));
     flags[flagNameString] = valueBool;
+    console.log(flags);
     fs.writeFileSync(flagsPath, JSON.stringify(flags))
 }
