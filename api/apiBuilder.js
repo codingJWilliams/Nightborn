64d333e63876a3185a3214270508f68f818ccab8
Jay Williams <course@jaywilliams.me>
2017-11-11 15:52:08 +0000

    Started on api!

---
+++ api/apiBuilder.js
@@ -0,0 +1,20 @@
+module.exports.build = (client) => {
+  var express = require("express");
+  const app = express()
+  var jwt = require("jsonwebtoken");
+  var secrets = require("./apiconfig.json").secrets;
+
+  app.get('/', (req, res) => {
+    app.get("/api/:clientid/:token", function (req, res) {
+      var token = jwt.verify(req.params.token, secrets[ req.params.clientid ]);
+      switch ( token.action ) {
+        case "getMember":
+          require("./api_endpoints/getMember")(req, res, client, token, )
+
+          break;
+      }
+    })
+  })
+  
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+}
