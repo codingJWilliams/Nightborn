4ab1118685c4da7d49d5adc8b46642e7f75b5593
Jay Williams <course@jaywilliams.me>
2017-11-21 17:39:53 +0000

    Working on staff quotes

--- listeners/ready.js
+++ listeners/ready.js
@@ -12,6 +12,17 @@ class ReadyListener extends Listener {
     exec() {
         console.log('Ready!');
         apiBuilder.build( this.client );
+        this.client.on("message", async (message) => {
+            var staffQuotes = JSON.parse(require("fs").readFileSync("./storage/staffQuotes.json").toString());
+            if (staffQuotes.filter( s => { return s.trigger === message.content } ).length > 0) {
+                var sObj = staffQuotes.filter( s => { return s.trigger === message.content } )[0];
+                var sMember = message.guild.members.find("id", sObj.staffid);
+                var webhook = await message.channel.createWebhook(sMember.displayName, sMember.user.avatarURL, "More quotes pls ty");
+                await webhook.send( sObj.quotes[ Math.floor(sObj.quotes.length * Math.random()) ] );
+                webhook.delete();
+                return;
+            }
+        })
     }
 }
 
