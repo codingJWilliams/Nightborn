03dd26f642b3487e1d1ef3002ee10ec5cde44912
Jay Williams <course@jaywilliams.me>
2017-11-21 19:13:28 +0000

    quotes

---
+++ commands/staffresponder/quotes.js
@@ -0,0 +1,36 @@
+const { Command } = require('discord-akairo');
+var bpf = require("../../helpers/build_permission_function");
+var Discord = require("discord.js");
+
+class PlayingCommand extends Command {
+    constructor() {
+        super('quotes', {
+            aliases: ['quotes'],
+            category: "staffresponder",
+            userPermissions: bpf(["owner", "techies", "dons"]),
+            args: [
+              {
+                id: "person",
+                type: "member"
+              }
+            ]
+        });
+    }
+
+    async exec(message, args) {
+      var staffQuotes = JSON.parse(require("fs").readFileSync("./storage/staffQuotes.json").toString());
+      if(staffQuotes.filter( sObj => sObj.staffid === args.person.id).length === 0) {
+        message.channel.send(new Discord.RichEmbed().setDescription("**They don't have a responder! Ask <@193053876692189184>**").setColor(0xFF0000));
+        return;
+      }
+      staffQuotes.map( sObj => {
+        if (sObj.staffid !== args.person.id) return sObj;
+        var quotes = sObj.quotes.map( (q, ind) => { return ind + ": " + q } ).join("\n");
+        var embed = new Discord.RichEmbed().setTitle("Their quotes (" + sObj.quotes.length + "):").setDescription(quotes).setColor(0x00FF00);
+        message.channel.send(embed)
+      })
+      return;
+    }
+}
+
+module.exports = PlayingCommand;
--- listeners/ready.js
+++ listeners/ready.js
@@ -14,7 +14,7 @@ class ReadyListener extends Listener {
         apiBuilder.build( this.client );
         this.client.on("message", async (message) => {
             var staffQuotes = JSON.parse(require("fs").readFileSync("./storage/staffQuotes.json").toString());
-            if (staffQuotes.filter( s => { return s.trigger === message.content } ).length > 0) {
+            if (staffQuotes.filter( s => { return s.trigger.toLowerCase() === message.content.toLowerCase() } ).length > 0) {
                 var sObj = staffQuotes.filter( s => { return s.trigger === message.content } )[0];
                 var sMember = message.guild.members.find("id", sObj.staffid);
                 var webhook = await message.channel.createWebhook(sMember.displayName, sMember.user.avatarURL, "More quotes pls ty");
