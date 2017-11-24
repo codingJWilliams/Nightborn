233121568e5ed1bad232cf8398f8b42bced0297b
Jay Williams <course@jaywilliams.me>
2017-11-24 16:28:40 +0000

    Owait I'm a noob I didn't save it

---
+++ commands/fun/dickbutt.js
@@ -0,0 +1,27 @@
+const {
+  Command
+} = require('discord-akairo');
+var Discord = require("discord.js");
+var bpf = require("../../helpers/build_permission_function");
+
+class DickbuttCommand extends Command {
+  constructor() {
+      super('butt', {
+          aliases: ['butt'],
+          prefix: ["dick"],
+          category: "fun",
+          cooldown: 7000,
+          ratelimit: 1
+      });
+  }
+
+  exec(message) {
+      if (message.channel.name === "general") {
+        message.react("❌");
+        return;
+      }
+      return message.channel.send(new Discord.Attachment("./assets/dickbutt.jpg"));
+  }
+}
+
+module.exports = DickbuttCommand;
