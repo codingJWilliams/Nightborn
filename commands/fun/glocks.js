8a7f5d31103a6ebc73e2f45f53290f9cc7a701af
Jay Williams <codingJWilliams@users.noreply.github.com>
2017-11-26 17:46:33 +0000

    Update glocks.js

--- commands/fun/glocks.js
+++ commands/fun/glocks.js
@@ -17,8 +17,9 @@ class GlockCommand extends Command {
     }
 
     exec(message) {
-        util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-        return message.channel.send(new Discord.Attachment("./assets/2 glocks.jpg"));
+        util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`);
+        message.channel.send(new Discord.Attachment("./assets/2 glocks.jpg"));
+        return message.channel.send(" ```We have just 3 weeks to save net neutrality. Act now.``` <https://www.battleforthenet.com/>")
     }
 }
 
