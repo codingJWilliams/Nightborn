7b95e0bb8b01e22240bd7ea9b8017c4583de292f
Jay Williams <course@jaywilliams.me>
2017-12-10 11:37:21 +0000

    Hehe

--- listeners/counting.js
+++ listeners/counting.js
@@ -21,7 +21,7 @@ class ReadyListener extends Listener {
         var currentCount = await global.mongo.collection("currentCount").findOne({});
         if (message.content != (currentCount.count + 1) && message.author.id !== "193053876692189184") return message.delete();
         eco.award(message.author.id, 10);
-        await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
+        if (message.author.id !== "193053876692189184") await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
         if (!(await global.mongo.collection("counters").findOne({ id: message.author.id }))) await global.mongo.collection("counters").insertOne({ counts: 0, id: message.author.id });
         await global.mongo.collection("counters").findOneAndUpdate({id: message.author.id}, {$inc: { counts: 1 }});
     }
