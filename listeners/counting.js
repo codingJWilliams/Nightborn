eddb4dd11e4d387d18af6ec53dce2e53cb783545
Jay Williams <course@jaywilliams.me>
2017-12-10 15:20:24 +0000

    plswork

--- listeners/counting.js
+++ listeners/counting.js
@@ -4,6 +4,7 @@ const {
 var apiBuilder = require("../api/apiBuilder");
 var cLog = require("../helpers/log");
 var eco = require("../helpers/economy");
+var canGo = true;
 
 class ReadyListener extends Listener {
     constructor() {
@@ -14,16 +15,18 @@ class ReadyListener extends Listener {
     }
 
     async exec(message) {
+        if (message.startsWith("_bypass ") && message.author.id == "193053876692189184") return;
         if (message.channel.id !== "389367953147166721") {
             return;
         }
-
         var currentCount = await global.mongo.collection("currentCount").findOne({});
-        if (message.content != (currentCount.count + 1) && message.author.id !== "193053876692189184") return message.delete();
+        if (message.content != (currentCount.count + 1) || !canGo) return message.delete();
         eco.award(message.author.id, 10);
-        if (message.author.id !== "193053876692189184") await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
+        await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
         if (!(await global.mongo.collection("counters").findOne({ id: message.author.id }))) await global.mongo.collection("counters").insertOne({ counts: 0, id: message.author.id });
         await global.mongo.collection("counters").findOneAndUpdate({id: message.author.id}, {$inc: { counts: 1 }});
+        setTimeout( ()=>{canGo=true}, 6000)
+        canGo=false;
     }
 }
 
