9e70d9c21a7c12044a36bc0de1b8af9bbe8e27be
Jay Williams <course@jaywilliams.me>
2017-12-10 11:31:47 +0000

    Jdh

--- listeners/counting.js
+++ listeners/counting.js
@@ -10,22 +10,18 @@ class ReadyListener extends Listener {
         super('counting', {
             emitter: 'client',
             eventName: 'message'
-        });
+        })
     }
 
-    exec(message) {
-        if (message.channel.id != "389367953147166721") return;
-
+    async exec(message) {
+        if (message.channel.id !== "389367953147166721") {
+            return;
+        }
         var currentCount = await global.mongo.collection("currentCount").findOne({});
-
         if (message.content != (currentCount.count + 1)) return message.delete();
-
         eco.award(message.author.id, 100);
-
         await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
-
         if (!(await global.mongo.collection("counters").findOne({ id: message.author.id }))) await global.mongo.collection("counters").insertOne({ counts: 0, id: message.author.id });
-
         await global.mongo.collection("counters").findOneAndUpdate({id: message.author.id}, {$inc: { counts: 1 }});
     }
 }
