1d37fe51f94e1be225c14c4f19b9d03154391701
Jay Williams <course@jaywilliams.me>
2017-12-10 11:07:39 +0000

    gamuf

--- listeners/counting.js
+++ listeners/counting.js
@@ -15,12 +15,17 @@ class ReadyListener extends Listener {
 
     exec(message) {
         if (message.channel.id !== "389367953147166721") return;
+
         var currentCount = await global.mongo.collection("currentCount").findOne({});
+
         currentCount = currentCount.count;
-        if (message.content != (currentCount + 1)) return message.delete();
-        eco.award(message.author.id, 100)
+        if (message.content != (currentCount + 1)) return await message.delete();
+        eco.award(message.author.id, 100);
+
         await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
+
         if (!(await global.mongo.collection("counters").findOne({ id: message.author.id }))) await global.mongo.collection("counters").insertOne({ counts: 0, id: message.author.id });
+        
         await global.mongo.collection("counters").findOneAndUpdate({id: message.author.id}, {$inc: { counts: 1 }});
     }
 }
