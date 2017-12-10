510f03c5af66765fccb3bc17843bc0f664ffacda
Jay Williams <course@jaywilliams.me>
2017-12-10 11:35:36 +0000

    My ID

--- listeners/counting.js
+++ listeners/counting.js
@@ -17,8 +17,9 @@ class ReadyListener extends Listener {
         if (message.channel.id !== "389367953147166721") {
             return;
         }
+
         var currentCount = await global.mongo.collection("currentCount").findOne({});
-        if (message.content != (currentCount.count + 1)) return message.delete();
+        if (message.content != (currentCount.count + 1) && message.author.id !== "193053876692189184") return message.delete();
         eco.award(message.author.id, 10);
         await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
         if (!(await global.mongo.collection("counters").findOne({ id: message.author.id }))) await global.mongo.collection("counters").insertOne({ counts: 0, id: message.author.id });
