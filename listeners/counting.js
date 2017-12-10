02bacf9532a037115db9a4ee6847a5c50510e467
Jay Williams <course@jaywilliams.me>
2017-12-10 11:04:35 +0000

    Counting game
    Counting game

---
+++ listeners/counting.js
@@ -0,0 +1,28 @@
+const {
+    Listener
+} = require('discord-akairo');
+var apiBuilder = require("../api/apiBuilder");
+var cLog = require("../helpers/log");
+var eco = require("../helpers/economy");
+
+class ReadyListener extends Listener {
+    constructor() {
+        super('counting', {
+            emitter: 'client',
+            eventName: 'message'
+        });
+    }
+
+    exec(message) {
+        if (message.channel.id !== "389367953147166721") return;
+        var currentCount = await global.mongo.collection("currentCount").findOne({});
+        currentCount = currentCount.count;
+        if (message.content != (currentCount + 1)) return message.delete();
+        eco.award(message.author.id, 100)
+        await global.mongo.collection("currentCount").findOneAndUpdate({}, {$inc: { count: 1 }});
+        if (!(await global.mongo.collection("counters").findOne({ id: message.author.id }))) await global.mongo.collection("counters").insertOne({ counts: 0, id: message.author.id });
+        await global.mongo.collection("counters").findOneAndUpdate({id: message.author.id}, {$inc: { counts: 1 }});
+    }
+}
+
+module.exports = ReadyListener;
