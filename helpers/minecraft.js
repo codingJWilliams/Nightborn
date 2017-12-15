2e576b56c570335a51dacdd4cf1ea24b5b99ccbc
Jay Williams <course@jaywilliams.me>
2017-12-15 20:01:50 +0000

    Docstringed minecraft functions

--- helpers/minecraft.js
+++ helpers/minecraft.js
@@ -2,7 +2,14 @@ var minecraft_raw = require("./minecraft_raw");
 var config = require("../config.json");
 var util = require("../helpers/util");
 
+/**
+ * Calls a method from http://mcjsonapi.org
+ * @param {string} method The method to call, from http://mcjsonapi.org
+ * @param {Array|undefined} args The arguments, if any.
+ */
 module.exports.easyCall = async function (method, args) {
+  if (args == undefined) args = [];
+  args = args ? args : []
   var res = await minecraft_raw.call({
     username: config.mc.username,
     password: config.mc.password,
@@ -12,6 +19,12 @@ module.exports.easyCall = async function (method, args) {
   });
   return res[0];
 }
+
+/**
+ * Takes a string, like "&cHello" and replaces & with § so colours show in Minecraft chat
+ * @param {string} s The string to escape
+ * @returns {string} The escaped string
+ */
 module.exports.colorCode = function (s) {
   return s.replace(/\&/g, "§");
 }
--- helpers/minecraft_raw.js
+++ helpers/minecraft_raw.js
@@ -14,6 +14,8 @@ module.exports.call = async function call(obj) {
   obj.key = getKey(obj.username, obj.name, obj.password, obj.salt);
   delete obj.password;
   delete obj.salt
-  var res = await axios.get("http://mc.nightborn.estate:25565/api/2/call?json=" + encodeURIComponent(JSON.stringify(obj)));
+  try { var res = await axios.get("http://mc.nightborn.estate:25565/api/2/call?json=" + encodeURIComponent(JSON.stringify(obj))); }
+  catch (e) { throw e }
+  
   return res.data;
 }
