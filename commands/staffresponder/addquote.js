12574d29c7ffef3eb843db0a2bf51a47c7532817
Jay Williams <course@jaywilliams.me>
2017-12-16 10:04:26 +0000

    Prettify & rename AddQuoteCommand

--- commands/staffresponder/addquote.js
+++ commands/staffresponder/addquote.js
@@ -4,8 +4,7 @@ const {
 var bpf = require("../../helpers/build_permission_function");
 var Discord = require("discord.js");
 var util = require("../../helpers/util");
-
-class PlayingCommand extends Command {
+class AddQuoteCommand extends Command {
   constructor() {
     super('addquote', {
       aliases: ['addquote'],
@@ -17,12 +16,16 @@ class PlayingCommand extends Command {
       }]
     });
   }
-
   async exec(message, args) {
     util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-    var staffQuotes = JSON.parse(require("fs").readFileSync("./storage/staffQuotes.json").toString());
-    if (staffQuotes.filter(sObj => sObj.staffid === message.author.id).length === 0) {
-      message.channel.send(new Discord.RichEmbed().setDescription("**You don't have a responder! Ask <@193053876692189184>**").setColor(0xFF0000));
+    var staffQuotes = JSON.parse(require("fs")
+      .readFileSync("./storage/staffQuotes.json")
+      .toString());
+    if (staffQuotes.filter(sObj => sObj.staffid === message.author.id)
+      .length === 0) {
+      message.channel.send(new Discord.RichEmbed()
+        .setDescription("**You don't have a responder! Ask <@193053876692189184>**")
+        .setColor(0xFF0000));
       return;
     }
     var staffQuotes = staffQuotes.map(sObj => {
@@ -30,10 +33,12 @@ class PlayingCommand extends Command {
       sObj.quotes.push(args.quote);
       return sObj;
     })
-    require("fs").writeFileSync("./storage/staffQuotes.json", JSON.stringify(staffQuotes, null, 2));
-    message.channel.send(new Discord.RichEmbed().setDescription("**Added quote!**").setColor(0x00FF00))
+    require("fs")
+      .writeFileSync("./storage/staffQuotes.json", JSON.stringify(staffQuotes, null, 2));
+    message.channel.send(new Discord.RichEmbed()
+      .setDescription("**Added quote!**")
+      .setColor(0x00FF00))
     return;
   }
 }
-
-module.exports = PlayingCommand;
+module.exports = AddQuoteCommand;
