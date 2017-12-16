032cdb4cda323e439d57c2e4ecdd9d666e2a8af4
Jay Williams <course@jaywilliams.me>
2017-12-16 09:26:15 +0000

    Fix DB allow IP command

--- commands/utility/allowdb.js
+++ commands/utility/allowdb.js
@@ -3,24 +3,21 @@ const {
 } = require('discord-akairo');
 var bpf = require("../../helpers/build_permission_function");
 var util = require("../../helpers/util");
-
-class SayCommand extends Command {
+class DbAllowBuilderCommand extends Command {
   constructor() {
-      super('allowdb', {
-          aliases: ['allowdb'],
-          split: "none",
-          category: "utility",
-          userPermissions: bpf(["owner", "techies", "dons"]),
-          args: [{
-              id: "a"
-          }]
-      });
+    super('allowdb', {
+      aliases: ['allowdb'],
+      split: "none",
+      category: "utility",
+      userPermissions: bpf(["owner", "techies", "dons"]),
+      args: [{
+        id: "a"
+      }]
+    });
   }
-
   async exec(message, args) {
-      util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-      message.reply(" set up! Run: `sudo ufw allow from "+ args.a +"/32 to any port 27017` in console to complete!")
+    util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
+    message.reply(" set up! Run: `sudo ufw allow from " + args.a + "/32 to any port 27017` in console to complete!")
   }
 }
-
-module.exports = SayCommand;
+module.exports = DbAllowBuilderCommand;
