d88f11162ae98c2c1429dbc53201625d387f5cc9
Jay Williams <course@jaywilliams.me>
2017-12-16 09:52:45 +0000

    Beautify PurgeCommand

--- commands/utility/purgebots.js
+++ commands/utility/purgebots.js
@@ -1,50 +1,46 @@
 //nb.eval var q = [];  clanless.map( (m) => { q.push( () => { m.addRole(message.guild.roles.get( "name", " )) } ) } )
 const {
-    Command
+  Command
 } = require('discord-akairo');
 var Discord = require("discord.js");
 var bpf = require("../../helpers/build_permission_function");
 var util = require("../../helpers/util");
-
 class PurgeCommand extends Command {
-    constructor() {
-        super('purgebots', {
-            aliases: ['purgebots'],
-            userPermissions: bpf(["owner", "techies", "dons", "mods", "intern"]),
-            category: "utility",
-            args: [{
-                id: "lookBack",
-                type: "number",
-                default: 100
-            }]
-        });
+  constructor() {
+    super('purgebots', {
+      aliases: ['purgebots'],
+      userPermissions: bpf(["owner", "techies", "dons", "mods", "intern"]),
+      category: "utility",
+      args: [{
+        id: "lookBack",
+        type: "number",
+        default: 100
+      }]
+    });
+  }
+  async exec(message, args) {
+    util.log("command." + this.id, "warn", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
+    var purgeAmount = args.lookBack;
+    try {
+      await message.react("🕒");
+      var fetched = await message.channel.fetchMessages({
+        amount: purgeAmount
+      })
+      await message.channel.bulkDelete(fetched.filter(m => {
+        return m.author.bot
+      }))
+      await message.clearReactions()
+      await message.react("✅");
+      setTimeout(() => {
+        message.delete()
+      }, 1000)
+    } catch (e) {
+      var m = await message.reply(`Couldn't delete messages because of: ${error}`);
+      setTimeout(() => {
+        m.delete();
+        message.delete()
+      }, 5000)
     }
-
-    async exec(message, args) {
-        util.log("command." + this.id , "warn", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-        var purgeAmount = args.lookBack;
-        try {
-            await message.react("🕒");
-            var fetched = await message.channel.fetchMessages({
-                amount: purgeAmount
-            })
-            await message.channel.bulkDelete(
-                fetched.filter(m => {
-                    return m.author.bot
-                })
-            )
-            await message.clearReactions()
-            await message.react("✅");
-            setTimeout(() => {
-                message.delete()
-            }, 1000)
-        } catch (e) {
-            var m = await message.reply(`Couldn't delete messages because of: ${error}`);
-            setTimeout(() => {
-                m.delete();
-                message.delete()
-            }, 5000)
-        }
-    }
+  }
 }
 module.exports = PurgeCommand;
