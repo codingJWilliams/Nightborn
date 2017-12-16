6dc8b15c547c12fd8f4e0a6d4e4d14c55830d59e
Jay Williams <course@jaywilliams.me>
2017-12-16 17:48:38 +0000

    Updated PurchaseCommand

--- commands/minecraft/purchase.js
+++ commands/minecraft/purchase.js
@@ -18,28 +18,26 @@ function getHash(str) {
   shasum.update(str);
   return shasum.digest("hex");
 }
-class PingCommand extends Command {
+class PurchaseCommand extends Command {
   constructor() {
     super('purchase', {
       aliases: ["purchase", "buy"],
       category: "minecraft",
       args: [{
-          id: "item",
-          type: "string"
-        },
-        {
-          id: "type",
-          type: "string"
-        }
-      ]
+        id: "item",
+        type: "string"
+      }, {
+        id: "type",
+        type: "string"
+      }]
     });
   }
-
   async exec(message, args) {
     util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-    var shopItem = await global.mongo.collection("shop").findOne({
-      id: args.item
-    });
+    var shopItem = await global.mongo.collection("shop")
+      .findOne({
+        id: args.item
+      });
     if (!shopItem) return await message.channel.send({
       embed: {
         title: "I could not find that item",
@@ -52,9 +50,10 @@ class PingCommand extends Command {
         color: 0xFF0000
       }
     });
-    var mcName = await global.mongo.collection("link").findOne({
-      discordID: message.author.id
-    });
+    var mcName = await global.mongo.collection("link")
+      .findOne({
+        discordID: message.author.id
+      });
     if (!mcName) return await message.channel.send({
       embed: {
         description: "Try re-linking and if that doesn't work, dm VoidCrafted",
@@ -63,21 +62,22 @@ class PingCommand extends Command {
       }
     })
     mcName = mcName.mcUsername;
-
     var theirBal = await economy.getBal(message.author.id);
     var cost = shopItem[args.type === "once" ? "costOnce" : "costPermanant"]
-    if (!cost) return message.channel.send("You cannot buy that item with that time option")    
+    if (!cost) return message.channel.send("You cannot buy that item with that time option")
     var command = shopItem[args.type === "once" ? "commandOnce" : "commandPermanant"].replace("$(player)", mcName);
     if (cost > theirBal) return await message.channel.send({
       embed: {
         description: "**<@" + message.author.id + "> You do not have enough souls for that**",
-        color: 0xFF0000
+        color: util.red
       }
     });
     var online = await mc.easyCall("players.online.names", []);
     online = online.success;
-
-    if (!online.includes(mcName)) return message.channel.send(new Discord.RichEmbed().setTitle("Couldn't find that player").setDescription("Hey, it looks like you're not online! Make sure you've spelt your name right and you're currently logged in!").setColor(0xFF0000));
+    if (!online.includes(mcName)) return message.channel.send(new Discord.RichEmbed()
+      .setTitle("Cannot find you!")
+      .setDescription("Huh, I couldn't see you on the server. You can try:\n - Re-linking your Minecraft\n - Making sure your name is still `"+ player +"`\n - DMing VoidCrafted")
+      .setColor(util.red));
     await economy.take(message.author.id, cost);
     await mc.easyCall("server.run_command", [
       command
@@ -89,10 +89,8 @@ class PingCommand extends Command {
     await message.channel.send(new Discord.RichEmbed()
       .setTitle("Successfully purchased!")
       .setThumbnail("https://crafatar.com/avatars/" + mcName + ".png")
-      .setColor(0x00FF00)
-      .setFooter("" + cost, "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/ghost_1f47b.png")
-    )
+      .setColor(util.green)
+      .setFooter("" + cost, "https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/118/ghost_1f47b.png"))
   }
 }
-
-module.exports = PingCommand;
+module.exports = PurchaseCommand;
