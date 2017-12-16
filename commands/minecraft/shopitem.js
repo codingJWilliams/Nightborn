dc3469fa451030ff2eddbc63754d9fc5a9589208
Jay Williams <course@jaywilliams.me>
2017-12-16 17:42:51 +0000

    Added support for permissions shop items

--- commands/minecraft/shopitem.js
+++ commands/minecraft/shopitem.js
@@ -45,7 +45,10 @@ class PingCommand extends Command {
         itemsStr = itemsStr + `**${item.name === item.item ? item.name : item.name + " {" + item.item + "}"}**${item.enchants.length ? `\n  Enchants: ${item.enchants.join(", ")}` : ""}${item.amount > 1 ? "\n  Amount: " + item.amount + "\n" : "\n"}`
       }
       if (item.type === "eco") {
-        itemsStr = itemsStr + "**"+ item.name +"**"
+        itemsStr = itemsStr + "**"+ item.name +"**\n"
+      }
+      if (item.type === "permission") {
+        itemsStr = itemsStr + `**${item.display}**\n - \`${item.permission}\`` + "\n"
       }
     })
     emb.addField("Items", itemsStr)
