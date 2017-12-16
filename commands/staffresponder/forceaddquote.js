4ff22e2e4d3deff431f8ec3c8383c53756a42f78
Jay Williams <course@jaywilliams.me>
2017-12-16 17:24:39 +0000

    Add order so economy is at botom

--- commands/staffresponder/forceaddquote.js
+++ commands/staffresponder/forceaddquote.js
@@ -4,7 +4,6 @@ const {
 var bpf = require("../../helpers/build_permission_function");
 var Discord = require("discord.js");
 var util = require("../../helpers/util");
-
 class PlayingCommand extends Command {
   constructor() {
     super('forceaddquote', {
@@ -12,23 +11,25 @@ class PlayingCommand extends Command {
       category: "staffresponder",
       split: "none",
       args: [{
-          "id": "person",
-          "type": "member"
-        },
-        {
-          "id": "quote",
-          "type": "string",
-          "match": "rest"
-        }
-      ]
+        "id": "person",
+        "type": "member"
+      }, {
+        "id": "quote",
+        "type": "string",
+        "match": "rest"
+      }]
     });
   }
-
   async exec(message, args) {
     util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-    var staffQuotes = JSON.parse(require("fs").readFileSync("./storage/staffQuotes.json").toString());
-    if (staffQuotes.filter(sObj => sObj.staffid === args.person.id).length === 0) {
-      message.channel.send(new Discord.RichEmbed().setDescription("**They don't have a responder! Ask <@193053876692189184>**").setColor(0xFF0000));
+    var staffQuotes = JSON.parse(require("fs")
+      .readFileSync("./storage/staffQuotes.json")
+      .toString());
+    if (staffQuotes.filter(sObj => sObj.staffid === args.person.id)
+      .length === 0) {
+      message.channel.send(new Discord.RichEmbed()
+        .setDescription("**They don't have a responder! Ask <@193053876692189184>**")
+        .setColor(0xFF0000));
       return;
     }
     var staffQuotes = staffQuotes.map(sObj => {
@@ -36,10 +37,12 @@ class PlayingCommand extends Command {
       sObj.quotes.push(args.quote);
       return sObj;
     })
-    require("fs").writeFileSync("./storage/staffQuotes.json", JSON.stringify(staffQuotes, null, 2));
-    message.channel.send(new Discord.RichEmbed().setDescription("**Added quote!**").setColor(0x00FF00))
+    require("fs")
+      .writeFileSync("./storage/staffQuotes.json", JSON.stringify(staffQuotes, null, 2));
+    message.channel.send(new Discord.RichEmbed()
+      .setDescription("**Added quote!**")
+      .setColor(0x00FF00))
     return;
   }
 }
-
 module.exports = PlayingCommand;
