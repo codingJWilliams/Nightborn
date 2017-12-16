97c2b20bbb8b0725f043f8cd13ef5cd65f47f465
Jay Williams <course@jaywilliams.me>
2017-12-16 09:26:48 +0000

    Rename DieCommand, beautify

--- commands/utility/die.js
+++ commands/utility/die.js
@@ -1,29 +1,29 @@
 const {
-    Command
+  Command
 } = require('discord-akairo');
 var bpf = require("../../helpers/build_permission_function");
 var economy = require("../../helpers/economy");
 var Discord = require("discord.js");
 const util_ = require('util');
-const exec = util_.promisify(require('child_process').exec);
+const exec = util_.promisify(require('child_process')
+  .exec);
 var util = require("../../helpers/util");
-
-class SayCommand extends Command {
-    constructor() {
-        super('die', {
-            aliases: ['die'],
-            category: "utility",
-            userPermissions: bpf(["owner", "dons", "techies"])
-        });
-    }
-
-    async exec(message) {
-        util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`);
-        util.log("command." + this.id, "warn", `Quitting. Byeeeeeee`)
-        message.channel.send(new Discord.RichEmbed().setDescription("Byeeee").setColor(0xFFFF00));
-        exec("pm2 stop NB");
-        return;
-    }
+class DieCommand extends Command {
+  constructor() {
+    super('die', {
+      aliases: ['die'],
+      category: "utility",
+      userPermissions: bpf(["owner", "dons", "techies"])
+    });
+  }
+  async exec(message) {
+    util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`);
+    util.log("command." + this.id, "warn", `Quitting. Byeeeeeee`)
+    message.channel.send(new Discord.RichEmbed()
+      .setDescription("Byeeee")
+      .setColor(0xFFFF00));
+    exec("pm2 stop NB");
+    return;
+  }
 }
-
-module.exports = SayCommand;
+module.exports = DieCommand;
