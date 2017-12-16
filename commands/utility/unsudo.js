fad840006cc522ef29e4faabd4eac338c5bacd14
Jay Williams <course@jaywilliams.me>
2017-12-16 09:58:34 +0000

    Beautify & Rename UnSudoCommand

--- commands/utility/unsudo.js
+++ commands/utility/unsudo.js
@@ -5,21 +5,18 @@ var bpf = require("../../helpers/build_permission_function");
 var economy = require("../../helpers/economy");
 var Discord = require("discord.js");
 var util = require("../../helpers/util");
-
-class SayCommand extends Command {
+class UnSudoCommand extends Command {
   constructor() {
-      super('unsudo', {
-          aliases: ['unsudo'],
-          category: "utility",
-          userPermissions: bpf([]),
-      });
+    super('unsudo', {
+      aliases: ['unsudo'],
+      category: "utility",
+      userPermissions: bpf([]),
+    });
   }
-
   async exec(message) {
-      util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-      message.guild.roles.map(r => r.name === "Sudo" ? r.delete() : false);
-      message.channel.send("Removed your sudo permissions.")
+    util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
+    message.guild.roles.map(r => r.name === "Sudo" ? r.delete() : false);
+    message.channel.send("Removed your sudo permissions.")
   }
 }
-
-module.exports = SayCommand;
+module.exports = UnSudoCommand;
