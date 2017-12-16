e9afc30c6373b37c8f8c225bc4aafc82add2d186
Jay Williams <course@jaywilliams.me>
2017-12-16 09:49:10 +0000

    Renamed class of MyIDCommand

--- commands/utility/myid.js
+++ commands/utility/myid.js
@@ -1,21 +1,18 @@
 const {
-    Command
+  Command
 } = require('discord-akairo');
 var bpf = require("../../helpers/build_permission_function");
 var util = require("../../helpers/util");
-
-class PingCommand extends Command {
-    constructor() {
-        super('myid', {
-            aliases: ['myid'],
-            category: "utility"
-        });
-    }
-
-    exec(message) {
-        util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-        return message.reply(' your ID is: `' + message.author.id + '`');
-    }
+class MyIDCommand extends Command {
+  constructor() {
+    super('myid', {
+      aliases: ['myid'],
+      category: "utility"
+    });
+  }
+  exec(message) {
+    util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
+    return message.reply(' your ID is: `' + message.author.id + '`');
+  }
 }
-
-module.exports = PingCommand;
+module.exports = MyIDCommand;
