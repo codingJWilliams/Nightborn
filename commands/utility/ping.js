20e5f84ca6462cef71218789b3b3958787bd58e2
Jay Williams <course@jaywilliams.me>
2017-12-16 09:51:34 +0000

    Beautify PingCommand

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,21 +1,18 @@
 const {
-    Command
+  Command
 } = require('discord-akairo');
 var bpf = require("../../helpers/build_permission_function");
 var util = require("../../helpers/util");
-
 class PingCommand extends Command {
-    constructor() {
-        super('ping', {
-            category: "utility",
-            aliases: ['ping', 'hello']
-        });
-    }
-
-    exec(message) {
-        util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-        return message.reply('Pong!');
-    }
+  constructor() {
+    super('ping', {
+      category: "utility",
+      aliases: ['ping', 'hello']
+    });
+  }
+  exec(message) {
+    util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
+    return message.reply('Pong!');
+  }
 }
-
 module.exports = PingCommand;
