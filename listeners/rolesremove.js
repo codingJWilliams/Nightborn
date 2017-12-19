ac2a123319dd2f3bcbe2cf861fc9a32caf61f65c
Jay Williams <course@jaywilliams.me>
2017-12-19 18:08:00 +0000

    aye

--- listeners/rolesremove.js
+++ listeners/rolesremove.js
@@ -12,7 +12,7 @@ class OptionalRolesListener extends Listener {
   async exec(messageReaction, user) {
     var channel = messageReaction.message.channel;
     //if (channel.name !== "welcome") return;
-    if (messageReaction.emoji !== "🚨" && messageReaction.emoji !== "⛔") return;
+    if (!(messageReaction.emoji == "🚨" || messageReaction.emoji == "⛔")) return;
     console.log(messageReaction.message.id)
     if (messageReaction.message.id !== "392738180761255936") return;
     var labBunnyRole = "392736357715542017";
