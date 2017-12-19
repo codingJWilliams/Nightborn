e1a78f224840bbd1d2a019627a1224329c261e4a
Jay Williams <course@jaywilliams.me>
2017-12-19 18:02:02 +0000

    ReactButtons

---
+++ listeners/roles.js
@@ -0,0 +1,30 @@
+const {
+  Listener
+} = require('discord-akairo');
+var cLog = require("../helpers/log");
+class OptionalRolesListener extends Listener {
+  constructor() {
+    super('optionalroles', {
+      emitter: 'client',
+      eventName: 'messageReactionAdd'
+    });
+  }
+  async exec(messageReaction, user) {
+    var channel = messageReaction.message.channel;
+    //if (channel.name !== "welcome") return;
+    if (messageReaction.emoji !== "🚨" && messageReaction.emoji !== "⛔") return;
+    if (messageReaction.message.id !== "392738180761255936") return;
+    var labBunnyRole = "392736357715542017";
+    var optoutRole = "392736650498932739";
+
+    if (messageReaction.emoji === "🚨") {
+      var member = await this.client.guilds.get("300155035558346752").fetchMember(user);
+      member.addRole(labBunnyRole)
+    }
+    else if (messageReaction.emoji === "⛔") {
+      var member = await this.client.guilds.get("300155035558346752").fetchMember(user);
+      member.addRole(optoutRole)
+    }
+  }
+}
+module.exports = BobListener;
