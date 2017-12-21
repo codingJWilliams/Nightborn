2f704cd924bbdacc2371c8ab5f851d5f355af907
Jay Williams <course@jaywilliams.me>
2017-12-21 16:22:10 +0000

    Better test structure

--- test/economy.test.js
+++ test/economy.test.js
@@ -1,14 +1,16 @@
 var should = require('should');
-var economy = require("../helpers/economy");
 
-describe("economy.get", ()=>{
-  it("should return a number", async () => {
-    var balance = await economy.getBal("193053876692189184");
-    balance.should.be.a.Number()
+describe("economy", ()=>{
+  describe("get", ()=>{
+    it("should return a number", async () => {
+      var balance = await economy.getBal("193053876692189184");
+      balance.should.be.a.Number()
+    })
+    it("should be a sensible number", async () => {
+      var balance = await economy.getBal("193053876692189184");
+      balance.should.be.aboveOrEqual(0)
+      should.equal(balance % 1, 0)
+    })
   })
-  it("should be a sensible number", async () => {
-    var balance = await economy.getBal("193053876692189184");
-    balance.should.be.aboveOrEqual(0)
-    should.equal(balance % 1, 0)
-  })
+  
 })
