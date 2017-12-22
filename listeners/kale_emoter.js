450b069ed053e485fd2739fb481a407342bbc5f1
Jay Williams <course@jaywilliams.me>
2017-12-22 20:23:23 +0000

    ayy

---
+++ listeners/kale_emoter.js
@@ -0,0 +1,24 @@
+const {
+  Listener
+} = require('discord-akairo');
+var apiBuilder = require("../api/apiBuilder");
+var cLog = require("../helpers/log");
+var eco = require("../helpers/economy");
+
+class ReadyListener extends Listener {
+  constructor() {
+    super('kale_emoter', {
+      emitter: 'client',
+      eventName: 'message'
+    })
+  }
+  async exec(message) {
+    if (message.author.id !== "200769142456844288") return;
+    // We're sure it's a message from kale
+    if (/<a:/.exec(message.content).length > 1) {
+      message.delete()
+      message.reply("stfu")
+    } 
+  }
+}
+module.exports = ReadyListener;
