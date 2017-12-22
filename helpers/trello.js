532e6d9fff65818e81856e609299a38fa299b6db
Jay Williams <course@jaywilliams.me>
2017-12-22 19:23:38 +0000

    Add tests for minecrat module

--- helpers/trello.js
+++ helpers/trello.js
@@ -7,17 +7,23 @@ var config = require("../config.json");
  * @param {Boolean} staff Whether the card was added by a staff member
  */
 exports.addCard = (name, desc, staff) => {
-  request({
-    uri: "https://api.trello.com/1/cards?key=" + config.trelloKey + "&token=" + config.trelloToken,
-    method: "POST",
-    json: {
-      name: name,
-      desc: desc,
-      pos: "top",
-      idList: "5a22eadd74cfce64c5181a8a",
-      idLabels: staff ? "5a22eabc7a03b6a878a691dd,5a22eac46ba09d840529af70" : "5a22eabc7a03b6a878a691dd"
-    }
-  }, function (err, resp, body) {
-    //console.log(body)
+  return new Promise((resolve, reject) => {
+    request({
+      uri: "https://api.trello.com/1/cards?key=" + config.trelloKey + "&token=" + config.trelloToken,
+      method: "POST",
+      json: {
+        name: name,
+        desc: desc,
+        pos: "top",
+        idList: "5a22eadd74cfce64c5181a8a",
+        idLabels: staff ? "5a22eabc7a03b6a878a691dd,5a22eac46ba09d840529af70" : "5a22eabc7a03b6a878a691dd"
+      }
+    }, function (err, resp, body) {
+      if (err) return reject(err)
+
+      resolve()
+      //console.log(body)
+    })
   })
+  
 }
---
+++ test/minecraft.test.js
@@ -0,0 +1,13 @@
+var should = require('should');
+var rewire = require("rewire");
+var minecraft = require("../helpers/minecraft")
+
+describe("minecraft", ()=>{
+  describe("colour escaper", ()=>{
+    it("should correctly escape example strings", ()=>{
+      minecraft.colorCode("&cTest").should.equal("§cTest")
+      minecraft.colorCode("&c").should.equal("§c")
+      minecraft.colorCode("abc&aabc").should.equal("abc§aabc")
+    })
+  })
+})
