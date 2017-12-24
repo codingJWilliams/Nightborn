9f3c651a93f89f58ebbdfaf715407a09b69818f9
Jay Williams <course@jaywilliams.me>
2017-12-24 10:26:12 +0000

    Add more logging

--- bot.js
+++ bot.js
@@ -81,3 +81,7 @@ MongoClient.connect(url, {
 })
 client.login(config.token);
 global.client = client
+
+client.on("error", (e) => console.error(e) && cLog("process.main", "error", e));
+client.on("warn", (e) => console.warn(e) && cLog("process.main", "warn", e));
+client.on("debug", (e) => console.info(e) && cLog("process.main", "debug", e));
