c94d596dacd509c8759e5d9d4e854a01688903fc
Jay Williams <course@jaywilliams.me>
2018-01-02 15:23:57 +0000

    Fix the damned thing

--- eventlisteners/message/nomic.js
+++ eventlisteners/message/nomic.js
@@ -1,6 +1,6 @@
 module.exports = async(msg) => {
   if (msg.channel.id !== "397763620307468298") return;
-  if (msg.author.roles.some(r => r.name === "staff")) return;
+  if (msg.member.roles.some(r => r.name === "staff")) return;
   setTimeout(()=>{
     msg.delete()
   }, 10 * 1000)
