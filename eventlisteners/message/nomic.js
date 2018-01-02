73b24135f223e1d1de94b4c19def0c739e5c1e7c
Jay Williams <course@jaywilliams.me>
2018-01-02 18:59:14 +0000

    Remove message deleter

--- eventlisteners/message/nomic.js
+++ eventlisteners/message/nomic.js
@@ -1,7 +1,9 @@
 module.exports = async(msg) => {
+  /*
   if (msg.channel.id !== "397763620307468298") return;
   if (msg.member.roles.some(r => r.name == "Staff")) return;
   setTimeout(()=>{
     msg.delete()
   }, 10 * 1000)
+  */
 }
