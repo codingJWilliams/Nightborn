2f46a5e81d5bfaaf72c05ce9bc56b99b1e9c7212
Jay Williams <codingJWilliams@users.noreply.github.com>
2018-01-06 20:15:15 +0100

    pls work kkty

--- commands/utility/streamer.js
+++ commands/utility/streamer.js
@@ -21,8 +21,13 @@ class StreamerCommand extends Command {
   async exec(message, args) {
     util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`);
     var streamerRole = "361591270759596043";
-    await (args.action === "add" ? args.person.addRole : args.person.removeRole)(streamerRole);
-    message.channel.send(new Discord.RichEmbed().setColor(0x4286f4).setDescription("**:white_check_mark: Successfully modified Streamer role of <@" + args.person.id + ">**"));
+    if (args.action === "add") {
+      args.person.addRole(streamerRole);
+      message.channel.send(new Discord.RichEmbed().setColor(0x4286f4).setDescription("**:white_check_mark: Successfully added Streamer role to <@" + args.person.id + ">**"));
+    } else {
+      args.person.removeRole(streamerRole);
+      message.channel.send(new Discord.RichEmbed().setColor(0x4286f4).setDescription("**:white_check_mark: Successfully removed Streamer role from <@" + args.person.id + ">**"));
+    }
   }
 }
 module.exports = StreamerCommand;
