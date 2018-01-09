db7d13611662729898889609e3c024bb5d26f744
Jay Williams <course@jaywilliams.me>
2018-01-09 18:50:42 +0000

    pls pls werk

--- commands/gambling/award.js
+++ commands/gambling/award.js
@@ -43,6 +43,7 @@ class AwardCommand extends Command {
     util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
     if (args.reciever.startsWith("<@&")) return message.channel.send(new Discord.RichEmbed().setTitle("Awarding to roles is not supported yet D:").setDescription("Sorry! It's coming soonTM").setColor(0xFF0000));
     try {
+      this.client.fetchUser(args.reciever.replace("<@", "").replace(">", ""))
       var user = await message.guild.fetchMember(args.reciever.replace("<@", "").replace(">", ""));
       await economy.award(user.id, args.amnt);
       message.channel.send(
