0c12056f2e8f04c461e4f2c24c193a1fda712805
Jay Williams <course@jaywilliams.me>
2018-01-12 20:19:18 +0000

    Added a command to get the current count

---
+++ commands/utility/count.js
@@ -0,0 +1,18 @@
+const {
+  Command
+} = require('discord-akairo');
+var bpf = require("../../helpers/build_permission_function");
+var util = require("../../helpers/util");
+class SayCommand extends Command {
+  constructor() {
+    super('count', {
+      aliases: ['count'],
+      category: "utility"
+    });
+  }
+  async exec(message, args) {
+    util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
+    message.reply("the next number you need to type is: " + (await global.mongo.collection("currentCount").findOne({})).count)
+  }
+}
+module.exports = SayCommand;
