0469f26ec0519b68d2e80614af0b691fc304d0ba
Jay Williams <course@jaywilliams.me>
2018-01-12 20:21:58 +0000

    Fix

--- commands/utility/count.js
+++ commands/utility/count.js
@@ -12,7 +12,7 @@ class SayCommand extends Command {
   }
   async exec(message, args) {
     util.log("command." + this.id, "cmd", `Executed by ${message.author.username}#${message.author.discriminator}, with message content ${message.content}`)
-    message.reply("the next number you need to type is: " + (await global.mongo.collection("currentCount").findOne({})).count)
+    message.reply("the next number you need to type is: " + ((await global.mongo.collection("currentCount").findOne({})).count + 1))
   }
 }
 module.exports = SayCommand;
