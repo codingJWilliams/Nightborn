c637028ca67f4c7f140f84f8f5319e338fed38dc
Jay Williams <course@jaywilliams.me>
2018-01-23 17:57:34 +0000

    FIX PLS

--- eventlisteners/message/dm.js
+++ eventlisteners/message/dm.js
@@ -1,4 +1,4 @@
 module.exports = async(message) => {
-  if (message.channel.type != "dm" && !message.author.bot) return;
+  if (message.channel.type != "dm" || message.author.id == global.client.user.id) return;
   message.channel.send("fok off")
 }
