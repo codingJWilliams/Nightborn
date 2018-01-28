a5a6b16a0f3a528563051620cb3466b8849f946c
Jay Williams <course@jaywilliams.me>
2018-01-28 15:33:20 +0000

    Fix loop

--- eventlisteners/guildMemberUpdate/nicknames.js
+++ eventlisteners/guildMemberUpdate/nicknames.js
@@ -1,6 +1,6 @@
 module.exports = async (oldM, newM) => {
   if (newM.nickname) {
-    if (newM.nickname.indexOf("★") !== 0) {
+    if (newM.nickname.indexOf("★") !== -1) {
       newM.setNickname(newM.nickname.replace(/★/g, ""));
     }
   }
