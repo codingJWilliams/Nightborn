4e23b3d9a7a04411c97364c974bd59af7939fc60
Jay Williams <course@jaywilliams.me>
2018-02-07 20:06:41 +0000

    Fix reeee leaky bot

--- listeners/bob.js
+++ listeners/bob.js
@@ -1,23 +1,24 @@
 const {
-  Listener
+    Listener
 } = require('discord-akairo');
 var cLog = require("../helpers/log");
 class BobListener extends Listener {
-  constructor() {
-    super('bob', {
-      emitter: 'client',
-      eventName: 'message'
-    });
-  }
-  exec(message) {
-    var blacklisted = ["staff", "admin"];
-    if (blacklisted.includes(message.channel.name)) return;
-    if (message.author.id !== "196870559630360576") return;
-    global.mongo.collection("bobquotes")
-      .insertOne({
-        quote: message.content,
-        time: Date.now()
-      })
-  }
+    constructor() {
+        super('bob', {
+            emitter: 'client',
+            eventName: 'message'
+        });
+    }
+    exec(message) {
+        var blacklisted = ["staff", "admin", "staff-pings", "moderation", "social-ops", "welcome-committee", "event-orgs", "botnet"];
+        if (blacklisted.includes(message.channel.name)) return;
+        if (message.author.id !== "196870559630360576") return;
+        global.mongo.collection("bobquotes")
+            .insertOne({
+                quote: message.content,
+                channel: message.channel.name,
+                time: Date.now()
+            })
+    }
 }
 module.exports = BobListener;
