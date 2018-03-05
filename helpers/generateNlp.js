d5838cea6b9d5db8e2a6d9c1c8ad942164715ad5
Jay Williams <course@jaywilliams.me>
2018-03-05 16:47:27 +0000

    Fix more

--- helpers/generateNlp.js
+++ helpers/generateNlp.js
@@ -4,5 +4,5 @@ const exec = util_.promisify(require('child_process')
 
 module.exports = async function() {
     var { stdout, stderr } = await exec("python3.5 ai/gen.py");
-    return stdout.replace("@everyone", "@not everyone").replace("@here", "@not here").replace(/<!?[0-9]+>/g, "@someone")
+    return stdout.replace("@everyone", "@not everyone").replace("@here", "@not here").replace(/\<!?[0-9]+\>/g, "@someone")
 }
