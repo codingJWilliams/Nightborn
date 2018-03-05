348dbca80976148f98a76298147051b97d4d54fa
Jay Williams <course@jaywilliams.me>
2018-03-05 16:24:44 +0000

    SCUFF

--- helpers/generateNlp.js
+++ helpers/generateNlp.js
@@ -3,6 +3,6 @@ const exec = util_.promisify(require('child_process')
     .exec);
 
 module.exports = async function() {
-    var { stdout, stderr } = await exec("python3 ai/gen.py");
+    var { stdout, stderr } = await exec("python3.5 ai/gen.py");
     return stdout.replace("@everyone", "@not everyone").replace("@here", "@not here").replace(/<@[0-9]+>/g, "")
 }
