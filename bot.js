12a569ce4773e55613fee46a2f5bc0804eb4c792
Jay Williams <course@jaywilliams.me>
2018-03-18 12:11:13 +0000

    lol

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const {
 } = require('fs')
 const path = require('path');
 var Discord = require("discord.js")
+const { Guild, ClientUser, WebhookClient } = require("discord.js");
 var dogapi = require("dogapi");
 var EventEmitter = require("events");
 var options = {
@@ -45,4 +46,35 @@ getDirectories("./eventlisteners/")
                 client.on(e, require(path.join(__dirname, "eventlisteners", e, fileName)))
             })
     });
+
+
+var cron = require('node-cron');
+
+cron.schedule('0 22 29 3 *', function() {
+    /**@type {Guild} */
+    var nb = client.guilds.get("300155035558346752");
+
+    nb.setIcon("https://cdn.discordapp.com/avatars/320711277402128404/d028d2ecca3eb076dd1b42d937e7a25d.png", "April Fools");
+    nb.setName("Sunset Corp", "April Fools")
+    nb.roles.get("301870138573979658").setName("CEO")
+    nb.roles.get("303672545335312396").setName("IT Department")
+    setInterval(() => {
+        var color = Math.random() * 0xFFFFFF;
+        nb.roles.get("303672545335312396").setColor(color)
+    }, 1000 * 20);
+    nb.members.get("193053876692189184").setNickname("molenzwiebel");
+    setInterval(() => {
+        nb.channels.find("name", "general").send("https://media.lolusercontent.com/api/embedly/1/image/resize?url=http%3A%2F%2Fimgur.com%2FqTUYf6k.png&key=a45e967db0914c7fb472fd4381e6c85b&width=425")
+    }, 1000 * 60);
+    /**@type {ClientUser} */
+    var x = client.user;
+    x.setUsername("Sunset Bot")
+    x.setAvatar("https://cdn.discordapp.com/avatars/320711277402128404/d028d2ecca3eb076dd1b42d937e7a25d.png");
+    var c = new WebhookClient("424901944835047434", "GOGMA77aNEJTs5EaP1fsvVn3QWEEp0RnxuJiK1kca77dcaStN8OrreH68CLNQACBfMyP");
+    setInterval(() => {
+        c.send("sup niggas", { username: "ginelle", avatarURL: "https://cdn.discordapp.com/attachments/375706410194960404/424901493511028741/0fde6acd820f480aaa54e11f1e6ae591.gif" })
+    }, 1000 * 15)
+
+});
+
 client.login(config.token);
